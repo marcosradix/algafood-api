c5e415948954519e733f41a7f1e3a9a02165cd9a
marcosradix <marcosradix@gmail.com>
2020-12-09 19:53:46 -0300

    fazendo fetch com jpql

--- src/main/java/br/com/workmade/domain/model/Restaurante.java
+++ src/main/java/br/com/workmade/domain/model/Restaurante.java
@@ -52,7 +52,7 @@ public class Restaurante {
 	@UpdateTimestamp
 	private LocalDate dataAtualizacao;
 
-	  @JsonIgnore
+	  //@JsonIgnore
 	  @ManyToMany
 	    @JoinTable(name="restaurante_pagamento", joinColumns=
 	    {@JoinColumn(name="restaurante_id")}, inverseJoinColumns=
--- src/main/java/br/com/workmade/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/workmade/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -19,6 +20,8 @@ public interface RestauranteRepository extends CustomJpaRepository<Restaurante, 
 	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 	//JPQL
 	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :cozinhaId")
+    @Query("from Restaurante r join r.cozinha left join fetch r.formaPagamentos")
+	List<Restaurante> findAll();
 	List<Restaurante> consultarPorNome(String nome, @Param("cozinhaId") Long cozinhaId);
 	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
